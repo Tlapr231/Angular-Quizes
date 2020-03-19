ac92df71a9e77a98801409b84c7a2ba5d6b4167c
Tlapr231 <tlapr093@uottawa.ca>
2020-03-19 11:35:56 -0400

    trying to generate api request

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { QuizDetailComponent } from './components/quiz-detail/quiz-detail.component';
 import { QuestionComponent } from './components/question/question.component';
 import { QuestionAnwserComponent } from './components/question-anwser/question-anwser.component';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/quiz', pathMatch: 'full' },
   // {path: 'home', component: HomeComponent},
   { path: 'quiz', component: QuizComponent },
+  { path: 'quiz/:id', component: QuizDetailComponent},
   { path: 'questions', component: QuestionComponent },
   { path: 'questiona/:id', component: QuestionAnwserComponent },
 ]
--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -5,6 +5,33 @@ import { catchError, map, tap } from "rxjs/operators";
 
 import { Quiz } from "./interface/quiz";
 
+const categoryId: {
+  "General Knowledge" : 9, 
+  "Entertainment: Books": 10, 
+  "Entertainment: Film" : 11,
+  "Entertainment: Music": 12,
+  "Entertainment: Musicals & Theatres": 13,
+  "Entertainment: Television": 14,
+  "Entertainment: Video Games": 15,
+  "Entertainment: Board Games": 16,
+  "Science & Nature": 17,
+  "Science: Computers": 18,
+  "Science: Mathematics": 19,
+  "Mythology": 20,
+  "Sports": 21,
+  "Geography": 22,
+  "History": 23,
+  "Politics": 24,
+  "Art": 25,
+  "Celebrities": 26,
+  "Animals": 27,
+  "Vehicles": 28,
+  "Entertainment: Comics": 29,
+  "Science: Gadgets": 30,
+  "Entertainment: Japanese Anime & Manga": 31,
+  "Entertainment: Cartoon & Animations": 32
+}
+
 @Injectable({
   providedIn: "root"
 })
@@ -24,13 +51,34 @@ export class QuizService {
     );
   }
 
+  addQuiz(numQuest: number, cate: string, diff: string): Observable<Quiz[]> {
+    let apiUrl = 'https://opentdb.com/api.php?amount=';
+    
+    apiUrl = apiUrl + numQuest;
+    if (cate === 'Any Category') {
+      apiUrl = apiUrl + '&category=' + categoryId[cate];
+    }  
+
+    if (diff === 'Any Difficulty'){
+      apiUrl = apiUrl + '&diffuculty=' + diff;
+    }
+
+    apiUrl = apiUrl + '&type=multiple';
+
+    return this.http.get<Quiz[]>(apiUrl).pipe(
+      tap(_ => this.log('Fetched from the api with : ' + apiUrl))
+    );
+
+
+  }
+
+
   //TODO (only going to use get quizes for testing for now)
   //Could implement more search Options later
   // getQuizByCategory(category: string): Observable<Quiz[]>{
 
   // }
 
-  // addQuiz()
 
   // deleteQuiz()
 
