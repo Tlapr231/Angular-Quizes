fbfd6a09b68b71461ea94553b655782f3b31a3fc
Tlapr231 <tlapr093@uottawa.ca>
2020-03-20 12:41:18 -0400

    Added mix anwser and code ready for anwsering

--- src/app/components/quiz-detail/quiz-detail.component.ts
+++ src/app/components/quiz-detail/quiz-detail.component.ts
@@ -26,6 +26,7 @@ export class QuizDetailComponent implements OnInit {
     if (+this.route.snapshot.paramMap.get('id') === 1000) {
       this.quizService.getQuiz(+this.route.snapshot.paramMap.get('id')).subscribe(quiz => {
         this.quiz = quiz
+        this.mixAnwsers();
         console.log(this.quiz);
       });
     } else {
@@ -33,5 +34,41 @@ export class QuizDetailComponent implements OnInit {
     }
     console.log(this.quiz);
   }
+
+  mixAnwsers() {
+    console.log("inside mixAnwser");
+    console.log(this.quiz);
+    for(var i = 1; i < this.quiz.questions.length; i++){
+      console.log(this.quiz.questions[i]);
+      let question = this.quiz.questions[i];
+      let anwser = [ question.correctAnw, question.incorrectAnw[0], question.incorrectAnw[1], question.incorrectAnw[2] ];
+      this.shuffle(anwser); 
+      question.anwser = anwser;
+      this.quiz.questions[i] = question;
+    }
+    console.log("Done with mixAnwser");
+  }
+
+  //======= Private Function ======//
+
+  //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  private shuffle(array: any[]) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
 
 }
